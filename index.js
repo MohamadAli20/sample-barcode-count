78af6a5e1b8e2aa3945861844349dd95efd335fc
Mohamad Ali Calanda <135398263+MohamadAli20@users.noreply.github.com>
2025-05-16 03:28:55 +0800

    Initial commit

---
+++ index.js
@@ -0,0 +1,47 @@
+/* Once QR is scanned, the result will be the value of input #scan-result */
+$('#scan-result').on('input', function () {
+    const result = this.value;
+    if (!result) return;              // nothing to do if empty
+
+    // split into individual parts
+    const [itemcode,
+        qty,
+        lotid,
+        expirationdate,
+        productiondate,
+        batchno] = result.split(';');
+
+    //// update your placeholders
+    //$('#p-itemcode').text(itemcode);
+    //$('#p-device').text('');
+    //$('#p-qty').text(qty.replace('QTY', ''));
+    //$('#p-lotid').text(lotid);
+    //$('#p-batchno').text(batchno);
+    //$('#p-productiondate').text(productiondate);
+    //$('#p-expirationdate').text(expirationdate.replace('ED', ''));
+
+    // build the new description block
+    const element = `
+        <div class="row row-data p-0 custom-radius border">
+            <div class="col text-center custom-col-qty">
+                <div class="">
+                    <div class="div-qty-value m-auto">${qty.replace('QTY', '') }</div>
+                    <div>RECEIVED QTY</div>
+                </div>
+            </div>
+            <div class="col p-0 custom-col-description">
+              <div class="div-description">
+                <p id="p-itemcode">${itemcode}</p>
+                <p id="p-device"></p>
+                <p>Qty:<span id="p-qty">${qty.replace('QTY', '')}</span></p>
+                <p id="p-lotid">${lotid}</p>
+                <p id="p-batchno">${batchno}</p>
+                <p>PD:<span id="p-productiondate">${productiondate}</span></p>
+                <p>ED:<span id="p-expirationdate">${expirationdate.replace('ED', '')}</span></p>
+              </div>
+            </div>
+        </div>`;
+
+    $('.row-scan-result').after(element);
+    $('#scan-result').val("");
+});
